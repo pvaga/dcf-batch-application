c75dd8db8590e84d16c5c06e8f82c2beba76bcb4
pvaga <pra.vaga@gmail.com>
2025-08-11 06:49:20 -0400

    Swagger UI enabled.

---
+++ src/main/java/gov/mass/dcf/batch/job/contoller/SpringBatchDetailController.java
@@ -0,0 +1,28 @@
+package gov.mass.dcf.batch.job.contoller;
+
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/batch")
+public class SpringBatchDetailController {
+
+    @Autowired
+    private JobExplorer jobExplorer;
+
+    @GetMapping("/job-executions")
+    public ResponseEntity<List<String>> getJobExecutions() {
+        List<String> executions = jobExplorer.getJobNames().stream()
+            .flatMap(jobName -> jobExplorer.findRunningJobExecutions(jobName).stream())
+            .map(JobExecution::toString)
+            .collect(Collectors.toList());
+        return ResponseEntity.ok(executions);
+    }
+}
